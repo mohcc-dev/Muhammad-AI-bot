d72460ac399d45dfe0000e8c3fb6714dc9a42229
mohcc-dev <mohcc7930@gmail.com>
2024-10-22 00:16:14 +0300

    Add files via upload

---
+++ ai Chat bot/main.js
@@ -0,0 +1,3 @@
+// API configuration
+const API_KEY = "Your_API_Key_Here"; // Your API key here
+const API_URL = `https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent?key=${API_KEY}`;
